50ecea2436f43eb1d5875eb8e66fd2178038de2e
Michael Lesniak <mlesniak@micromata.de>
2016-09-19 20:04:09 +0200

    Initial commit

---
+++ src/main/scala/com/mlesniak/github/Main.scala
@@ -0,0 +1,10 @@
+package com.mlesniak.github
+
+/**
+  * Application entry point.
+  *
+  * @author Michael Lesniak (mlesniak@micromata.de)
+  */
+object Main extends App {
+  println("Hello, world")
+}
