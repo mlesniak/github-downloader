a2f55283e4938f01284284cb06ea0ec50e797d6a
Michael Lesniak <mlesniak@micromata.de>
2016-09-19 20:11:00 +0200

    Parse start date

--- src/main/scala/com/mlesniak/github/Main.scala
+++ src/main/scala/com/mlesniak/github/Main.scala
@@ -1,10 +1,19 @@
 package com.mlesniak.github
 
+import java.text.SimpleDateFormat
+import java.util.Properties
+
 /**
   * Application entry point.
   *
   * @author Michael Lesniak (mlesniak@micromata.de)
   */
 object Main extends App {
-  println("Hello, world")
+  val props = new Properties()
+  props.load(getClass.getResourceAsStream("/date-ranges.properties"))
+
+  val sdf = new SimpleDateFormat("dd.MM.yyyy")
+  val startDate = sdf.parse(props.getProperty("start"))
+
+  println(startDate)
 }
