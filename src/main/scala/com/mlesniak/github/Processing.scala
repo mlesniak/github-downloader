c759e3bba50a9dc6c8d0ec65880d60bc2edee28e
Michael Lesniak <mlesniak@micromata.de>
2016-09-21 00:18:58 +0200

    Temporary comment spark to allow execution with java -jar

--- src/main/scala/com/mlesniak/github/Processing.scala
+++ src/main/scala/com/mlesniak/github/Processing.scala
@@ -1,6 +1,6 @@
 package com.mlesniak.github
 
-import org.apache.spark.sql.SparkSession
+//import org.apache.spark.sql.SparkSession
 
 /**
   * Processing playground, might be a separate project later on...
@@ -8,26 +8,28 @@ import org.apache.spark.sql.SparkSession
   * @author Michael Lesniak (mlesniak@micromata.de)
   */
 object Processing extends App {
-  val spark = SparkSession
-    .builder()
-    .appName("Spark SQL Example")
-    .master("local[*]")
-    // .config("spark.some.config.option", "some-value")
-    .getOrCreate()
+  //  val spark = SparkSession
+  //    .builder()
+  //    .appName("Spark SQL Example")
+  //    .master("local[*]")
+  //    // .config("spark.some.config.option", "some-value")
+  //    .getOrCreate()
+  //
+  //
+  //  val path = "data/2011-02-12-0.json"
+  //  val github = spark.read.json(path)
+  //
+  //  github.printSchema()
+  //  github.createOrReplaceTempView("github")
+  //  val repos = spark.sql("SELECT repo.name, count(repo.name) FROM github group by repo.name order by count(repo.name) " +
+  //    "desc")
+  //  repos.show()
+  //
+  //  val rows = repos.rdd
+  //  println(rows)
+  //
+  //  val grdd = github.rdd
+  //  println(grdd)
 
-
-  val path = "data/2011-02-12-0.json"
-  val github = spark.read.json(path)
-
-  github.printSchema()
-  github.createOrReplaceTempView("github")
-  val repos = spark.sql("SELECT repo.name, count(repo.name) FROM github group by repo.name order by count(repo.name) " +
-    "desc")
-  repos.show()
-
-  val rows = repos.rdd
-  println(rows)
-
-  val grdd = github.rdd
-  println(grdd)
+  // User, event, time
 }
