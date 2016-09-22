b6a55a5f1d702a278a810286690ee6125fcb3dac
Michael Lesniak <mlesniak@micromata.de>
2016-09-22 08:51:52 +0200

    Fix halting bug while writing user-specific files

--- src/main/scala/com/mlesniak/github/Processing.scala
+++ src/main/scala/com/mlesniak/github/Processing.scala
@@ -13,24 +13,29 @@ object Processing extends App {
   val spark = SparkSession
     .builder()
     .appName("Spark SQL Example")
-    .master("local[*]")
+    .master("local[1]")
     .getOrCreate()
 
 
   val path = "data/2011-02-12-0.json"
   val github = spark.read.json(path)
 
-  github.createOrReplaceTempView("github")
-  github.cache()
+  github.createTempView("github")
+  github.persist()
 
   val logins = spark.sql("select distinct actor.login from github")
-  logins.show()
+  logins.persist()
+  val loginCount = logins.count()
+  println(s"Number of logins $loginCount")
 
-  logins.foreach(row => {
-    // Store all data for a given user.
+  // See https://stackoverflow.com/questions/33030726/how-to-iterate-records-spark-scala for the motivation behind
+  // using collect().
+  // TODO ML Examine reason for collect() deeper.
+  logins.collect().foreach(row => {
     val username = row(0)
-    println(s"Processing $username")
-    val userData = github.filter(s"actor.login = '$username'")
+    println(s"Writing $username")
+    val userData = spark.sql(s"select * from github where actor.login = '$username'")
     userData.write.mode(SaveMode.Overwrite).json(s"data/out/$username")
   })
 }
+
